2b98d4da8f8284374a790ba7eb5ca4c55a9362b1
CharlieOuthwaite <charloteouthwaite@gmail.com>
2021-06-10 11:22:01 +0100

    Starting coding to explore Mark's maps

---
+++ 1_Hotspots_Multimetric_indicator/1_Hotspots_UKTradePartners_multmetric.R
@@ -0,0 +1,13 @@
+##%######################################################%##
+#                                                          #
+####              1. Multimetric indicator              ####
+####            hotspots, UK trade partners             ####
+#                                                          #
+##%######################################################%##
+
+
+# started by Charlie Outhwaite, 10/06/2021
+
+# Here, I am exploring Mark's multimetric indicator rasters. 
+# First, I will explore the countries that are major UK trade partners for 
+# our crops and will make some summaries of the indicators for these countries
