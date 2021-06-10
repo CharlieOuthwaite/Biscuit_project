5b004cc1d48d9dcd552db76ab4a46357e3321acf
CharlieOuthwaite <charloteouthwaite@gmail.com>
2021-06-10 15:00:49 +0100

    added getting country polygons

--- 1_Hotspots_Multimetric_indicator/1_Hotspots_UKTradePartners_multmetric.R
+++ 1_Hotspots_Multimetric_indicator/1_Hotspots_UKTradePartners_multmetric.R
@@ -51,17 +51,50 @@ length(countries) # currently 18 suppliers of interest
 
 
 
+#### Task 1: Summary stats for each country ####
+
+# For now, will just use the total impact raster files
+
+files <- list.files(paste0(datadir, "/Marks_Maps"), pattern = "_Total.tif")
+
+# 5 files
+# "GHG_Emissons_Total.tif"
+# "LD_BioDiv_Total.tif"
+# "N_Marine_BioDiv_Total.tif"
+# "P_Marine_BioDiv_Total.tif"
+# "Water_Debt_Total.tif" 
+
+# taking an initial look at Mark's maps
+
+# each file can be opened as a raster stack which has a band per crop (See table in README from Mark)
+
+# resolution: 0.08333333
+
+map1_stack <- stack(paste0(datadir, "/Marks_Maps/", files[1]))
+
+plot(map1[[2]])       
+
+res(map1)
+
+crs(map1)
 
 
+#### get country border data for the list of countries ####
+View(getData('ISO3')) 
+
+# get the country codes to extract country polygons
+codes <- getData('ISO3')
+
+# some names do not match so added in manually
+cntry_codes <- codes[codes$NAME %in% countries | codes$NAME == "United States" | codes$NAME == "Swaziland", ]
+
+# extract country shapefiles for 
+codes = list()
+codes$countries <- cntry_codes$ISO3
+
+# download and combine polygons into one object
+ctry_shps = do.call("bind", lapply(codes$countries, 
+                                   function(x) getData('GADM', country=x, level=0)))
 
 
-
-
-
-
-
-# ideas:
-
-# 1. look specifically at the area that is covered by each crop (use Earthstat maps)
-# and summarise the indicators for those crop specific areas. 
-
+#### get some summary stat for each country/indicator/crop combo ####
