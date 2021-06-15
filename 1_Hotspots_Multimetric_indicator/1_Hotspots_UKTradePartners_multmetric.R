f5dd3359b1f6e0d7d389b1193fca75b1dd39d1d8
CharlieOuthwaite <charloteouthwaite@gmail.com>
2021-06-15 14:58:37 +0100

    Extracted summary stats for all trade partners in list for each crop/indicator

--- 1_Hotspots_Multimetric_indicator/1_Hotspots_UKTradePartners_multmetric.R
+++ 1_Hotspots_Multimetric_indicator/1_Hotspots_UKTradePartners_multmetric.R
@@ -21,6 +21,9 @@ rm(list = ls())
 
 # load required libraries
 library(raster)
+library(rgdal)
+library(exactextractr) # exact_extract function from here
+
 
 # set directories
 datadir <- "Data"
@@ -37,7 +40,7 @@ list.files(paste0(datadir, "/Marks_Maps"), pattern = ".tif")
 
 # which countries are we interested in?
 # Use Carole's list of key trade partners for our crops of interest for 2003 (I think)
-# this is saved in the Google drive (currently missing cocoa - to be added hopefully today)
+# this is saved in the Google drive 
 
 # read in Carole's list
 suppliers <- read.csv(paste0(datadir, "/UK_Suppliers_main_crops_Carole.csv"))
@@ -47,7 +50,7 @@ View(suppliers)
 # get a list of countries (note this does not include cocoa at the moment)
 countries <- unique(suppliers$partner)
 
-length(countries) # currently 18 suppliers of interest
+length(countries) # currently 21 suppliers of interest
 
 
 
@@ -85,7 +88,7 @@ View(getData('ISO3'))
 codes <- getData('ISO3')
 
 # some names do not match so added in manually
-cntry_codes <- codes[codes$NAME %in% countries | codes$NAME == "United States" | codes$NAME == "Swaziland", ]
+cntry_codes <- codes[codes$NAME %in% countries | codes$NAME == "United States" | codes$NAME == "Swaziland" | codes$NAME == "Côte d'Ivoire", ]
 
 # extract country shapefiles for 
 codes = list()
@@ -95,10 +98,14 @@ codes$countries <- cntry_codes$ISO3
 ctry_shps = do.call("bind", lapply(codes$countries, 
                                    function(x) getData('GADM', country=x, level=0)))
 
+# this takes a little while to download all the polygons
+
+# take a little look
+plot(ctry_shps)
+plot(map1_stack[[1]], add = TRUE) 
 
 # save this polygons object for future use
-
-
+shapefile(ctry_shps, filename = paste0(outdir, "/Trade_partners_polygons.shp"))
 
 
 
@@ -112,11 +119,81 @@ ctry_shps = do.call("bind", lapply(codes$countries,
 
 # what other summaries would be good?
 
+#### 1. GHG Total indicator map ####
+
+# load in the raster stack
+GHG <- stack(paste0(datadir, "/Marks_Maps/", files[1]))
+
+# use exact_extract function to get some summary stats per country/band
+# the function does the same for each layer automatically
+
+GHG_sums <- exact_extract(x = GHG, y = ctry_shps, fun = c('mean', 'min', 'max', 'median', 'stdev'))
+
+# need to organise the outputted info
+rownames(GHG_sums) <- ctry_shps$NAME_0
+
+colnames(GHG_sums) <- sub("GHG_Emissons_Total.1", "Cocoa", colnames(GHG_sums))
+colnames(GHG_sums) <- sub("GHG_Emissons_Total.2", "Oilpalm", colnames(GHG_sums))
+colnames(GHG_sums) <- sub("GHG_Emissons_Total.3", "SugarBeet", colnames(GHG_sums))
+colnames(GHG_sums) <- sub("GHG_Emissons_Total.4", "SugarCane", colnames(GHG_sums))
+colnames(GHG_sums) <- sub("GHG_Emissons_Total.5", "Wheat", colnames(GHG_sums))
+
+#### 2. Land biodiversity impact ####
+
+LND <- stack(paste0(datadir, "/Marks_Maps/", files[2]))
+
+LND_sums <- exact_extract(x = LND, y = ctry_shps, fun = c('mean', 'min', 'max', 'median', 'stdev'))
+
+# need to organise the outputted info
+rownames(LND_sums) <- ctry_shps$NAME_0
+
+colnames(LND_sums) <- sub("GHG_Emissons_Total.1", "Cocoa", colnames(LND_sums))
+colnames(LND_sums) <- sub("GHG_Emissons_Total.2", "Oilpalm", colnames(LND_sums))
+colnames(LND_sums) <- sub("GHG_Emissons_Total.3", "SugarBeet", colnames(LND_sums))
+colnames(LND_sums) <- sub("GHG_Emissons_Total.4", "SugarCane", colnames(LND_sums))
+colnames(LND_sums) <- sub("GHG_Emissons_Total.5", "Wheat", colnames(LND_sums))
+
+#### 3. N biodiv impact ####
+
+Nit <- stack(paste0(datadir, "/Marks_Maps/", files[3]))
+
+Nit_sums <- exact_extract(x = Nit, y = ctry_shps, fun = c('mean', 'min', 'max', 'median', 'stdev'))
+
+# need to organise the outputted info
+rownames(Nit_sums) <- ctry_shps$NAME_0
+
+colnames(Nit_sums) <- sub("GHG_Emissons_Total.1", "Oilpalm", colnames(Nit_sums))
+colnames(Nit_sums) <- sub("GHG_Emissons_Total.2", "SugarBeet", colnames(Nit_sums))
+colnames(Nit_sums) <- sub("GHG_Emissons_Total.3", "SugarCane", colnames(Nit_sums))
+colnames(Nit_sums) <- sub("GHG_Emissons_Total.4", "Wheat", colnames(Nit_sums))
 
 
+#### 4. P biodiv impact ####
+
+Pho <- stack(paste0(datadir, "/Marks_Maps/", files[4]))
+
+Pho_sums <- exact_extract(x = Pho, y = ctry_shps, fun = c('mean', 'min', 'max', 'median', 'stdev'))
+
+# need to organise the outputted info
+rownames(Pho_sums) <- ctry_shps$NAME_0
+
+colnames(Pho_sums) <- sub("GHG_Emissons_Total.1", "Oilpalm", colnames(Pho_sums))
+colnames(Pho_sums) <- sub("GHG_Emissons_Total.2", "SugarBeet", colnames(Pho_sums))
+colnames(Pho_sums) <- sub("GHG_Emissons_Total.3", "SugarCane", colnames(Pho_sums))
+colnames(Pho_sums) <- sub("GHG_Emissons_Total.4", "Wheat", colnames(Pho_sums))
 
 
+#### 5. water debt ####
 
+WAT <- stack(paste0(datadir, "/Marks_Maps/", files[5]))
 
+WAT_sums <- exact_extract(x = WAT, y = ctry_shps, fun = c('mean', 'min', 'max', 'median', 'stdev'))
 
+# need to organise the outputted info
+rownames(WAT_sums) <- ctry_shps$NAME_0
+
+colnames(WAT_sums) <- sub("GHG_Emissons_Total.1", "Oilpalm", colnames(WAT_sums))
+colnames(WAT_sums) <- sub("GHG_Emissons_Total.2", "SugarBeet", colnames(WAT_sums))
+colnames(WAT_sums) <- sub("GHG_Emissons_Total.3", "SugarCane", colnames(WAT_sums))
+colnames(WAT_sums) <- sub("GHG_Emissons_Total.4", "Wheat", colnames(WAT_sums))
 
